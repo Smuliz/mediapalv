2f0efca1cf6b3c105f9bd0c25b65d54911eb3fb2
M4824 <M4824@student.jamk.fi>
2020-03-24 17:32:51 +0200

    http-b

--- src/components/catTable.js
+++ src/components/catTable.js
@@ -2,14 +2,20 @@ import React, {useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
 import CatRow from './catRow';
 
+const baseUrl = 'http://media.mw.metropolia.fi/wbma/';
+
 const CatTable = () => {
     //const {media} = props;
     const [picArray, setPicArray] = useState([]);
     const loadMedia = async () => {
-        const response = await fetch('test.json');
+        const response = await fetch(baseUrl + 'media');
         const json = await response.json();
+        const items = await Promise.all(json.map(async (item) => {
+            const response = await fetch(baseUrl + 'media/' + item.file_id);
+            return await response.json();
+        }));
         console.log(json);
-        setPicArray(json);
+        setPicArray(items);
     };
 
     useEffect(() => {
